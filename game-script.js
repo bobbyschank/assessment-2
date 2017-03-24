e0067056582f79ecf499a7bff1943c71f2bd6d32
bobbyschank <robertschank@gmail.com>
2017-03-23 22:13:35 -0600

    js

--- game-script.js
+++ game-script.js
@@ -1,27 +1,46 @@
 console.log("js in the hizzy");
 
 let meyer = document.getElementById('duncan');
+let meyerLocation = meyer.style.left;
+
 let jerry = document.getElementById('jerry');
+let jerryLocation = jerry.style.left;
+
+let jerryStride = document.getElementById('jerryStride');
+let duncanStride = document.getElementById('duncanStride');
 
 let jerryButton = document.getElementById('jerryButton');
-jerryButton.
+
+function Player(name, winMessage) {
+	this.name = name;
+	this.id = name.toLowerCase();
+	this.position = 0;
+	this.won = false;
+	this.winMessage = winMessage;
+	this.step = function() {
+		console.log("STEP");
+		let stride = randomGenerator();
+		this.position = this.position + stride;
+		document.getElementById(this.id).style.left = this.position + "px";
+		document.getElementById(this.id + "Stride").textContent = stride;
+		console.log(this.winMessage);
+		if (this.position > 500) {
+			this.won = true;
+			alert(this.winMessage);
+			location.reload();
+		}
+	};
+}
+
+let jerryPlayer = new Player("Jerry", "Jerry Wins! Yessss!");
+let duncanPlayer = new Player("Duncan", "You win this time Duncan.");
+
+jerryButton.addEventListener("click", function(){
+	console.log('RUN JERRY!!!!');
+	jerryPlayer.step();
+	duncanPlayer.step();
+});
 
 function randomGenerator() {
-  	return Math.round(Math.random() * (9)) + 1;
+  	return Math.round(Math.random() * (19)) + 1;
 }
-
-
-jerry.color = "red";
-
-let jerryLocation = jerry.style.left;
-console.log("jerryLocation:" + jerryLocation);
-
-function step() {
-	let randomInt = randomGenerator();
-	// guys[0].style.left = randomInt * 10;
-	// guys[0].style.background-color = "blue";
-
-console.log(randomInt);
-}
-
-step();
