162d6cb98a35aaf3ae0abfd506c8655c8766a3d9
bobbyschank <robertschank@gmail.com>
2017-03-26 16:34:29 -0600

    media queries

--- game-script.js
+++ game-script.js
@@ -1,48 +1,35 @@
-console.log("js in the hizzy");
-
-let meyer = document.getElementById('duncan');
-let meyerLocation = meyer.style.left;
-
-let jerry = document.getElementById('jerry');
-let jerryLocation = jerry.style.left;
-
-let jerryStride = document.getElementById('jerryStride');
-let duncanStride = document.getElementById('duncanStride');
-
 let jerryButton = document.getElementById('jerryButton');
 
 function Player(name, winMessage) {
 	this.name = name;
 	this.id = name.toLowerCase();
-	this.position = 0;
-	this.won = false;
-	this.winMessage = winMessage;
+	this.position = 0;              				// player starts on left side of section
+	this.winMessage = winMessage;  					// what the alert will display when the game ends
 	this.element = document.getElementById(name);
-	console.log(this.element);
 	this.step = function() {
-		console.log("STEP");
 		let stride = randomGenerator();
 		this.position = this.position + stride;
 		document.getElementById(this.id).style.left = this.position + "px";
 		document.getElementById(this.id + "Stride").textContent = stride;
-		console.log(this.winMessage);
-		if (this.position > 767) {
-			this.won = true;
+		let containerWidth = document.getElementById('container').offsetWidth;  // if either div reaches the edge of the section, end the game
+		let finishLine = containerWidth - (35 + 6);  // find container width, and subtract the width of the guys div
+		if (this.position > finishLine) {
 			alert(this.winMessage);
-			location.reload();
+			location.reload();  				//reload page after game end, and after user acknowledges alert
 		}
 	};
 }
 
+// Instantiate the players
 let jerryPlayer = new Player("Jerry", "Jerry Wins! Yessss!");
 let duncanPlayer = new Player("Duncan", "Ok Duncan, you win this time.");
 
 jerryButton.addEventListener("click", function(){
-	console.log('RUN JERRY!!!!');
+	// Each time the button is clicked, jerry and duncan each take a step
 	jerryPlayer.step();
 	duncanPlayer.step();
 });
 
 function randomGenerator() {
-  	return Math.round(Math.random() * (29)) + 1;
+  	return Math.round(Math.random() * (29)) + 1;    // generates random integer between 1 and 30
 }
